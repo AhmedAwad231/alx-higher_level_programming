69f7d9d056c6f69ca2e9cf00cb5446f934020417
ahmed <ahmed@example.com>
2023-11-27 11:03:26 -0800

    script

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,36 @@
-#include <stdio.h>
+/*
+ * File: 10-check_cycle.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdlib.h>
 #include "lists.h"
-/***
- * Check_cycle -  checks if list is cyclical
- * @list: pointer to list to check
- * Return: 1 if cyclical. 0 otherwise
- */
-int int check_cycle(listint_t *list)
-{
-	listint_t *slow = list, *fast = list;
 
-	while (fast && fast->next)
+/**
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		if (turtle == hare)
 			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
+
 	return (0);
 }
